70d1e1a486f2131620b95adde38e34c13a6a0dbe
Matt F Zhang <mzhang294@nam.ad.pwcinternal.com>
2019-11-22 14:05:33 +0800

    office

--- sample-spring-microservices-logstash/gateway-service/src/main/java/pl/piomin/microservices/edge/Application.java
+++ sample-spring-microservices-logstash/gateway-service/src/main/java/pl/piomin/microservices/edge/Application.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.SpringApplication;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -17,7 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class Application {
 
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(Application.class).web(true).run(args);
+		//new SpringApplicationBuilder(Application.class).web(true).run(args);
+		 SpringApplication.run(Application.class, args);
 	}
 
 	@Bean
--- sample-spring-microservices-logstash/gateway-service/src/main/java/pl/piomin/microservices/edge/PreFilter.java
+++ sample-spring-microservices-logstash/gateway-service/src/main/java/pl/piomin/microservices/edge/PreFilter.java
@@ -1,17 +1,24 @@
 package pl.piomin.microservices.edge;
 
 import javax.servlet.http.HttpServletRequest;
-
+//import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 
+//@Slf4j
 public class PreFilter extends ZuulFilter {
   
   protected static Logger logger = LoggerFactory.getLogger(PreFilter.class.getName());
-	
+  
+   @Autowired
+   private StringRedisTemplate stringRedisTemplate;
+   @Autowired
+   private org.springframework.cloud.sleuth.Tracer tracer;
   @Override
   public String filterType() {
     return "pre";
@@ -29,9 +36,24 @@ public class PreFilter extends ZuulFilter {
 
   @Override
   public Object run() {
+//	  org.springframework.cloud.sleuth.Span newSpan = tracer.createSpan("redis");
+//	  try {
+//	  	newSpan.tag("redis.op", "get");
+//	  	newSpan.tag("lc", "redis");
+//	  	newSpan.logEvent(org.springframework.cloud.sleuth.Span.CLIENT_SEND);
+//	  	// call redis service e.g
+//		stringRedisTemplate.opsForValue().set("aaa", "111");  
+//	  } finally {
+//	  	newSpan.tag("peer.service", "redisService");
+//	  	newSpan.tag("peer.ipv4", "127.0.0.1");
+//	  	newSpan.tag("peer.port", "1234");
+//	  	newSpan.logEvent(org.springframework.cloud.sleuth.Span.CLIENT_RECV);
+//	  	tracer.close(newSpan);
+//	  }
+	stringRedisTemplate.opsForValue().set("aaa", "111");  
     RequestContext ctx = RequestContext.getCurrentContext();
     HttpServletRequest request = ctx.getRequest();
-    logger.debug("PreFilter Request Method : " + request.getMethod() + " Request URL : " + request.getRequestURL().toString());
+    logger.info("PreFilter Request Method : " + request.getMethod() + " Request URL : " + request.getRequestURL().toString());
     return null;
   }
 
